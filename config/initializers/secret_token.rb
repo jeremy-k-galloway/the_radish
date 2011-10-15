95ce55c91a961714f302b35d975447f2d05b2652
Jeremy Galloway <jeremy.k.galloway@gmail.com>
2011-10-14 23:22:42 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheRadish::Application.config.secret_token = 'f244188a41d22b38659d123f3eb103dd0aac406250f474ec4764f8e932030b58c959e07b0c5c0e259704b0ff9d3e5d2d891dcd3f099041b2c1150496fd45c1ac'
